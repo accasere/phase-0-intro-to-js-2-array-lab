d99cd348d71207b3d1d619afcfd4e30c9e653b8d
Antony Caseres <accasere@outlook.com>
2022-03-20 18:23:59 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+let cats = ["Milo","Otis", "Garfield"]
+
+function destructivelyAppendCat(name = 'Ralph'){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name){
+    let copyOfCats = [...cats, name]
+    return copyOfCats
+}
+
+function prependCat(name) {
+    let copyOfCats= [name,...cats]
+    return copyOfCats
+}
+
+function  removeLastCat() {
+    let copyOfCats = cats.slice(0,2) 
+    return copyOfCats
+}
+
+function removeFirstCat() {
+    let copyOfCats = cats.slice(1) 
+    return copyOfCats
+}
